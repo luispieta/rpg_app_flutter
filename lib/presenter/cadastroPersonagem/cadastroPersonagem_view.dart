fb2f49cb2cc7508a126e8ea6023352000f94de27
luispieta <lufelipepieta@gmail.com>
2025-10-28 21:29:39 -0300

    Feat: Melhorias no campo de seleção e no cadastro de personagem

--- lib/presenter/cadastroPersonagem/cadastroPersonagem_view.dart
+++ lib/presenter/cadastroPersonagem/cadastroPersonagem_view.dart
@@ -2,8 +2,62 @@ import 'package:flutter/material.dart';
 import 'package:rpg_app/presenter/cadastroPersonagem/campoSelcao.dart';
 import 'package:rpg_app/presenter/cadastroPersonagem/cards.dart';
 
-class CadastropersonagemView extends StatelessWidget {
+class CadastropersonagemView extends StatefulWidget {
   const CadastropersonagemView({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _CadastropersonagemViewState();
+}
+
+class _CadastropersonagemViewState extends State<CadastropersonagemView> {
+
+  String racaSelecionada = "Escolha uma raça";
+  String arquetipoSelecionada = "Escolha um arquétipo";
+
+  final racas = ["Escolha uma raça", "Humano", "Android", "Alienígena"];
+  final arquetipos = ["Escolha um arquétipo", "Samurai", "Hacker", "Cibernético"];
+
+  _imagemSelecionada () {
+    if (racaSelecionada == "Humano" && arquetipoSelecionada == "Escolha um arquétipo") {
+      return "assets/images/Humano.png";
+
+    } else if (racaSelecionada == "Android" && arquetipoSelecionada == "Escolha um arquétipo") {
+      return "assets/images/android.png";
+
+    } else if (racaSelecionada == "Alienígena" && arquetipoSelecionada == "Escolha um arquétipo") {
+      return "assets/images/alienigena.png";
+
+    } else if (racaSelecionada == "Humano" && arquetipoSelecionada == "Samurai") {
+      return "assets/images/humano_samurai.png";
+
+    } else if (racaSelecionada == "Humano" && arquetipoSelecionada == "Hacker") {
+      return "assets/images/humano_hacker.png";
+
+    } else if (racaSelecionada == "Humano" && arquetipoSelecionada == "Cibernético") {
+      return "assets/images/humano_cibernetico.png";
+
+    } else if (racaSelecionada == "Android" && arquetipoSelecionada == "Samurai") {
+      return "assets/images/android_samurai.png";
+
+    } else if (racaSelecionada == "Android" && arquetipoSelecionada == "Hacker") {
+      return "assets/images/android_hacker.png";
+
+    } else if (racaSelecionada == "Android" && arquetipoSelecionada == "Cibernético") {
+      return "assets/images/android_cibernetico.png";
+
+    }  else if (racaSelecionada == "Alienígena" && arquetipoSelecionada == "Samurai") {
+      return "assets/images/alienigena_samurai.png";
+
+    } else if (racaSelecionada == "Alienígena" && arquetipoSelecionada == "Hacker") {
+      return "assets/images/alienigena_hacker.png";
+
+    } else if (racaSelecionada == "Alienígena" && arquetipoSelecionada == "Cibernético") {
+      return "assets/images/alienigena_cibernetico.png";
+
+    } else {
+      return "assets/images/humano.png";
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,8 +70,31 @@ class CadastropersonagemView extends StatelessWidget {
       body: SingleChildScrollView(
         child: Column(
           children: [
-            const CampoSelecao(), 
+             Image.asset(
+              _imagemSelecionada(),
+            ),
+
+            CampoSelecao(
+              texto: "Raça", 
+              listas: racas, 
+              textoSelecionado: racaSelecionada,
+              onChange: (value) { 
+                setState(() => racaSelecionada = value!
+              );},
+            ),
+            
+            CampoSelecao(
+              texto: "Arquétipo", 
+              listas: arquetipos, 
+              textoSelecionado: arquetipoSelecionada,
+              onChange: (value) { 
+                setState(() => arquetipoSelecionada = value!
+              );},
+            ),
+
             const Cards(texto: "Vida", pontos: 50),
+            const Cards(texto: "Escudo", pontos: 50),
+            const Cards(texto: "Velocidade", pontos: 50),
           ] 
         )
       ) 
--- lib/presenter/cadastroPersonagem/campoSelcao.dart
+++ lib/presenter/cadastroPersonagem/campoSelcao.dart
@@ -1,62 +1,20 @@
 import 'package:flutter/material.dart';
 
-class CampoSelecao extends StatefulWidget {
-  const CampoSelecao({super.key});
+class CampoSelecao extends StatelessWidget{
+  final String texto;
+  final List listas;
+  final String textoSelecionado;
+  final ValueChanged<String?> onChange;
 
-  @override
-  State<CampoSelecao> createState() => _CampoSelecaoState();
-}
+  const CampoSelecao({
+    super.key,
+    required this.texto,
+    required this.listas,
+    required this.textoSelecionado,
+    required this.onChange
+  });
 
-class _CampoSelecaoState extends State<CampoSelecao> {
-
-  final racas = ["Escolha uma raça", "Humano", "Android", "Alienígena"];
-  final arquetipos = ["Escolha um arquétipo", "Samurai", "Hacker", "Cibernético"];
-
-  String? racaSelecionada = "Escolha uma raça";
-  String? arquetipoSelecionada = "Escolha um arquétipo";
-
-  _imagemSelecionada () {
-    if (racaSelecionada == "Humano" && arquetipoSelecionada == "Escolha um arquétipo") {
-      return "assets/images/Humano.png";
-
-    } else if (racaSelecionada == "Android" && arquetipoSelecionada == "Escolha um arquétipo") {
-      return "assets/images/android.png";
-
-    } else if (racaSelecionada == "Alienígena" && arquetipoSelecionada == "Escolha um arquétipo") {
-      return "assets/images/alienigena.png";
-
-    } else if (racaSelecionada == "Humano" && arquetipoSelecionada == "Samurai") {
-      return "assets/images/humano_samurai.png";
-
-    } else if (racaSelecionada == "Humano" && arquetipoSelecionada == "Hacker") {
-      return "assets/images/humano_hacker.png";
-
-    } else if (racaSelecionada == "Humano" && arquetipoSelecionada == "Cibernético") {
-      return "assets/images/humano_cibernetico.png";
-
-    } else if (racaSelecionada == "Android" && arquetipoSelecionada == "Samurai") {
-      return "assets/images/android_samurai.png";
-
-    } else if (racaSelecionada == "Android" && arquetipoSelecionada == "Hacker") {
-      return "assets/images/android_hacker.png";
-
-    } else if (racaSelecionada == "Android" && arquetipoSelecionada == "Cibernético") {
-      return "assets/images/android_cibernetico.png";
-
-    }  else if (racaSelecionada == "Alienígena" && arquetipoSelecionada == "Samurai") {
-      return "assets/images/alienigena_samurai.png";
-
-    } else if (racaSelecionada == "Alienígena" && arquetipoSelecionada == "Hacker") {
-      return "assets/images/alienigena_hacker.png";
-
-    } else if (racaSelecionada == "Alienígena" && arquetipoSelecionada == "Cibernético") {
-      return "assets/images/alienigena_cibernetico.png";
-
-    } else {
-      return "assets/images/humano.png";
-
-    }
-  }
+  String get pronome => texto == "Raça" ? "uma" : "um";
 
   @override
   Widget build(BuildContext context) {
@@ -64,57 +22,23 @@ class _CampoSelecaoState extends State<CampoSelecao> {
       padding: const EdgeInsets.all(16.0),
         child: Column(
           children: [
-            // Imagem atual
-            Image.asset(
-              _imagemSelecionada(),
-            ),
-
-            const SizedBox(height: 20),
-
             DropdownButtonFormField<String>(
-              decoration: const InputDecoration(
-                labelText: "Raça",
-                border: OutlineInputBorder(),
+              decoration: InputDecoration(
+                labelText: texto,
+                border: const OutlineInputBorder(),
               ),  
-              hint: const Text("Escolha uma raça"), 
-              value: racaSelecionada, 
-              items: racas.map((raca) {
+              hint: Text("Escolha $pronome $texto"), 
+              value: textoSelecionado, 
+              items: listas.map((lista) {
                 return DropdownMenuItem<String>(
-                  value: raca,
-                  child: Text(raca),
+                  value: lista,
+                  child: Text(lista),
                 );
               }).toList(),
-              onChanged: (String? newValue) {
-                setState(() {
-                  racaSelecionada = newValue;
-                });
-              },
-            ),
-
-            const SizedBox(height: 20),
-
-            DropdownButtonFormField<String>(
-              decoration: const InputDecoration(
-                labelText: "Arquétipo",
-                border: OutlineInputBorder(),
-              ),  
-              hint: const Text("Escolha um arquétipo"), 
-              value: arquetipoSelecionada, 
-              items: arquetipos.map((arquetipo) {
-                return DropdownMenuItem<String>(
-                  value: arquetipo,
-                  child: Text(arquetipo),
-                );
-              }).toList(),
-              onChanged: (String? newValue) {
-                setState(() {
-                  arquetipoSelecionada = newValue;
-                });
-              },
+              onChanged: onChange,
             ),
           ]
       ),
     );
   }
-
 }
